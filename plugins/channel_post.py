aa72e5fb13d80c9b7e9a662a44836c082d0e8a0c
nobita66 <152915019+nobita66@users.noreply.github.com>
2024-02-14 00:20:24 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -8,6 +8,8 @@ from pyrogram.errors import FloodWait
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
+import requests
+
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
@@ -25,10 +27,37 @@ async def channel_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
+    api_url = "https://publicearn.com/api"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    # API token
+    api_token = "85d2cf5838d6c742c6a855eb514af076ea5c3790"
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    # Destination URL to shorten
+    destination_url = "{link}"
+
+    # Alias for the shortened URL (optional)
+    alias = "CustomAlias"
+
+    # Format for the response (text)
+    response_format = "text"
+
+    # Construct the request URL
+    request_url = f"{api_url}?api={api_token}&url={destination_url}&alias={alias}&format={response_format}"
+
+    # Send the GET request
+    response = requests.get(request_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+    # Extract the shortened URL from the response
+    shortened_url = response.text.strip()
+    print(f"Shortened URL: {shortened_url}")
+    else:
+    print("Failed to generate shortened URL. Status code:", response.status_code)
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={shortened_url}')]])
+
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{shortened_url}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
@@ -42,8 +71,8 @@ async def new_post(client: Client, message: Message):
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    shortened_url = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={shortened_url}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
